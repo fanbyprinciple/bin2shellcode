c2d45e30e333b6603da6fd391884479ce6956422
fanbyprinciple <fanbyprinciple@gmail.com>
2023-01-13 16:39:31 +0530

    adding code for c and cs

---
+++ bin2sc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys
+if __name__ == "__main__":
+	if len(sys.argv) < 3:
+		print "usage: %s file.bin c|cs\n" % (sys.argv[0],)
+		sys.exit(0)
+
+	if sys.argv[3] == "c":
+		# for c shellcode
+		shellcode = "\""
+		ctr = 1
+		maxlen = 15
+
+		for b in open(sys.argv[1], "rb").read():
+			shellcode += "\\x" + b.encode("hex")
+			if ctr == maxlen:
+				shellcode += "\" +\n\""
+				ctr = 0
+			ctr += 1
+		shellcode += "\""
+		print shellcode
+
+	else:
+		# for cs shellcode
+		shellcode = ""
+		ctr = 1
+		maxlen = 15
+		
+		for b in open(sys.argv[1], "rb").read():
+			shellcode += "0x" + b.encode("hex") + ","
+			if ctr == maxlen:
+				shellcode += "\n"
+				ctr = 0
+			ctr += 1
+
+		print shellcode
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Convert .bin to c# and c shellcode
+
+Usage:
+
+```
+python2 bin2sc.py <file>.bin c|cs
+```
+
+Credits :
+1.  @superkojiman for his c shellcode conversion.
+https://gist.github.com/superkojiman/11164279
